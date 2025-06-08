1d70d7e918c956eb49c3121cba4bee331a474021
shanid1 <shanidktk18@gmail.com>
2025-06-08 21:17:02 +0530

    Initial commit with React Firebase app

--- thrift-store/src/App.jsx
+++ thrift-store/src/App.jsx
@@ -10,9 +10,6 @@ import Footer from "./Footer";
 import ProductView from "./ProductView";
 import About from "./About";
 
-import ad2 from '/assets/ad2.png';
-import ad1 from '/assets/ad1.png';
-import ad3 from '/assets/ad3.webp';
 import "./App.css";
 import { query } from "firebase/firestore";
 
@@ -90,12 +87,14 @@ function App() {
 
       {showMain && <div className="mainAd">
       <div className="mobileAd" style={{display:"flex", justifyContent:"center"}}>
-     <Section imgurl={ad1}width="60%"title="Hot Pics"goSection={() => handleSection("Hot")} />
-     <Section imgurl={ad2} width="30%"title="RetroJERSEY"goSection={() => handleSection("Jersey")} />
+     <Section imgurl={"/assets/ad1.png"} width="60%" title="Hot Pics" goSection={() => handleSection("Hot")} />
+<Section imgurl={"/assets/ad2.png"} width="30%" title="RetroJERSEY" goSection={() => handleSection("Jersey")} />
+
+
       </div>
       <Banner/>
       <All mainTitle={"Top Selling"} name={username} showView={handleView}/>
-      <Section imgurl={ad3} width="95%"title="Trending Pants"goSection={() => handleSection("Pants")} />
+      <Section imgurl={"/assets/ad3.webp"} width="95%" title="Trending Pants" goSection={() => handleSection("Pants")} />
         <About/>
         </div>}
         <div>
